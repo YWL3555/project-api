776b14a6c7db9b66348c3d872d4ffabd393f0474
Yong Wen Li <ykk3555@gmail.com>
2020-02-27 06:55:57 +0800

    Update codes

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import tourGuide
+from .serializers import tourGuideSerializer
 
 # Create your views here.
+
+class tourGuideView(viewsets.ModelViewSet):
+    queryset = tourGuide.objects.all()
+    serializer_class = tourGuideSerializer
