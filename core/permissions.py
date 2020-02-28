cc6e3e3c777cabfd1cc1c0ccfb45da4c924f22f2
Yong Wen Li <ykk3555@gmail.com>
2020-02-28 11:38:03 +0800

    Create new permissions

--- core/permissions.py
+++ core/permissions.py
@@ -16,3 +16,12 @@ class IsTourGuide(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_tourGuide)
+
+
+class IsStudentOrTourGuide(permissions.BasePermission):
+    """
+    Allows access only to users with student role.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and (request.user.is_student or request.is_tourGuide))
